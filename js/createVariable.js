b673bf704f9bf5f46c7e0878771731bb903473d1
Alirio A. Angel <alirioangel.1993@gmail.com>
2018-07-25 05:08:34 -0400

    Modificacion de las tablas/adicion de los controladores de crearVariable y Editar formula

---
+++ js/createVariable.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('#create_variable_button').click(function(){
+        var nombre = $('#nombre_variable').val();
+        var valor = $('#valor_variable').val();
+        $('#status-create-variable').html('');
+        $('#status-create-variable2').html('');
+        if (nombre.trim() == '') {
+            $('#status-create-variable').html('<span style="color:red;">Por Favor ingrese el nombre de la variable.</span>');
+            $('#nombre_variable').focus();
+            return false
+        }
+        else if(valor.trim()== ''){
+            $('#status-create-variable2').html('<span style="color:red;">Por Favor ingrese el valor de la variable.</span>');
+            $('#valor_variable').focus();
+            return false
+        }
+        else{
+            $.ajax({
+                type:   "POST",
+                url:    "createVariable.php",
+                data: {nombreVariable:nombre,valorVariable:valor},
+                beforeSend: function() {
+                    $('#create_variable_button').html('creando...');
+                    $('#create_variable_button').attr("disabled","disabled");
+                },
+                success: function(msg){
+                    if(msg=='ok'){
+                        console.log('Creacion exitosa');
+                        $('#nombre_variable').val('');
+                        $('#valor_variable').val('');
+                        setTimeout(() => {
+                            $('#create_variable_button').removeAttr("disabled");
+                            $('#create_variable_button').html('crear');
+                            setTimeout(() => {
+                                window.location = 'configurar.php';
+                            }, 500);
+
+                        }, 1000);
+                    }else{
+                        console.log('Error!');
+                        $('#statusVariable').html('<span style="color:red;">Ha ocurrido un error!.</span>')
+                        $('#create_variable_button').removeAttr("disabled");
+                        $('#create_variable_button').html('crear');
+                    }
+                }
+            })
+        }
+    })
+})
--- verTabla1.php
+++ verTabla1.php
@@ -62,12 +62,22 @@
 							<a role="button" href="./calcular.php" class="btn btn-outline-secondary">Calcular el bono de alimentacion</a>
 						</div>
 					</div>
+
 					<br>
-					<h3 class="d-flex justify-content-center">Escoge la Tabla a ver</h3>
+                    <h3 class="d-flex justify-content-center">Escoge la Tabla a ver</h3>
 					<br>
 					<div class="row">
 						<div class="col-md-6"><a role="button" href="./vertabla1.php" class="btn btn-block btn-outline-primary">Trabajadores Administrativos/obreros</a></div>
 						<div class="col-md-6"><a role="button" href="./verTabla2.php" class="btn btn-block btn-outline-primary">Profesores</a></div>
+					</div>
+                    <br>
+                    <br>
+                    <h3 class="d-flex justify-content-center">Tabla de Trabajadores Administrativos/ Obreros de la UJAP</h3>
+                    <br>
+					<div class="row">
+					<?php
+						include 'tabla.php';
+					?>		
 					</div>
 				</div>
       </div>
@@ -76,7 +86,7 @@
     <script src="./js/popper.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
     <script src="./js/modal.js"></script>
-    <!--<script src="./js/paginacion.js"></script>-->
+    <script src="./js/paginacion.js"></script>
     <script src="./js/busqueda.js"></script>
 	</body>
 </html>
